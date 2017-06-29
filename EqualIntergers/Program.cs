ecc8c5a080b5d1e265d675dd529258a9a095957a
Jerome <jscar216@gmail.com>
2017-06-29 09:21:14 -0400

    EqualIntergers

--- EqualIntergers/Program.cs
+++ EqualIntergers/Program.cs
@@ -11,21 +11,120 @@ namespace EqualIntergers
         static void Main(string[] args)
         //Asking user to type in two numbers to see if they are equal 
         {
-            Console.WriteLine("Please enter a number!");
-            int number = int.Parse(Console.ReadLine());
-            Console.WriteLine("Please enter a second number!");
-            int secondNumber = int.Parse(Console.ReadLine());
-        //See if numbers are equal 
-            if (number == secondNumber)
+            //    Console.WriteLine("Please enter a number!");
+            //    int number = int.Parse(Console.ReadLine());
+            //    Console.WriteLine("Please enter a second number!");
+            //    int secondNumber = int.Parse(Console.ReadLine());
+            ////See if numbers are equal 
+            //    if (number == secondNumber)
+            //    {
+            //        Console.WriteLine("Numbers" + number +"and" + secondNumber + "are equal");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Numbers" + number +  "and" + secondNumber + "are not equal");
+            //    }
+
+
+            // Even or Odd
+
+
+            //Ask user for a number
+            //Console.WriteLine("Welcome to Odd or Even!");
+
+            //Console.WriteLine("Please enter a number!");
+            //int number = int.Parse(Console.ReadLine());
+            ////See if numbers are odd or even
+            //if (number % 2 == 0)
+            //{
+            //    Console.WriteLine("Number entered is Even!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Number entered is Odd!");
+            //}
+
+            //Positive or Negative
+            //Ask user to type a number
+            //Tell wheter its positive or negative
+            //User inputs zero results in error
+
+            //    Console.WriteLine("Positive or Negative!");
+            //    Console.WriteLine("Please enter a number!");
+            //    int number = int.Parse(Console.ReadLine());
+
+            //    if (number <= 0)
+            //    {
+
+            //        Console.WriteLine("The number is negative");
+            //    }
+            //    if (number == 0)
+            //    { 
+            //        Console.WriteLine("Error! You entered Zero its niether Positve or Negative!");
+            //}
+            //    if (number >= 0)
+            //        {
+            //        Console.WriteLine("The number is positve");
+            //    }
+            //    else
+            //    //cannot figure how to make zero stand alone
+            //    {
+
+            //    }
+
+            //Vowel or Consonant
+            //Create char variable for if-else-if statements
+            //Ask user to type a letter
+            //Make sure to have both lower and upper case vowels and consonants
+
+            //variable created
+            char letter = 'x';
+
             {
-                Console.WriteLine("Numbers" + number +"and" + secondNumber + "are equal");
+                Console.WriteLine("Please type a letter!");
+                Console.ReadLine();
+            }
+            //figure this out later something wrong with code
+              else if (letter == 'a' || letter == 'A')
+            {
+                Console.WriteLine("The letter is a vowel!");
+            }
+            else if (letter == 'e'  || letter =='E')
+            {
+                Console.WriteLine("The letter is a vowel!");
+            }
+            else if (letter == 'i' || letter == 'I')
+            {
+                Console.WriteLine("The letter is a vowel!");
+            }
+            else if (letter == 'o' || letter == 'O')
+            {
+                Console.WriteLine("The letter is a vowel!");
+            }
+            else if (letter == 'u' || letter == 'U')
+            {
+                Console.WriteLine("The letter is a vowel!");
             }
             else
             {
-                Console.WriteLine("Numbers" + number +  "and" + secondNumber + "are not equal");
+                Console.WriteLine("The letter is a consonant!");
             }
+
+
+
+
+
+
+            }
+
+
+
+
+
+
+
 
 
         }
     }
-}
+
